c656d56c29956c68aacb9c2aff02bb7684b8fba2
Nayarjay <74157421+Nayarjay@users.noreply.github.com>
2023-06-09 22:01:56 +0200

    movement

--- Assets/Script/cat/PlayerMovement.cs
+++ Assets/Script/cat/PlayerMovement.cs
@@ -10,9 +10,15 @@ public class PlayerMovement : MonoBehaviour
     float honrizontalMove = 0f;
     bool jump = false;
     public Animator animator;
+    public float dashForce = 10f;
+
+    private Rigidbody2D rb;
+    private Transform selfTransform;
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        selfTransform = transform;
     }
 
     // Update is called once per frame
@@ -33,10 +39,28 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            animator.SetBool("IsJumping", false);
+            animator.Play("Rush");
+
+
+            // Calcul de la direction de dash en fonction de la rotation de l'objet lui-même
+            Vector2 dashDirection = selfTransform.right;
+
+            // Appliquer la force de dash au Rigidbody de l'objet
+            rb.AddForce(dashDirection * dashForce, ForceMode2D.Impulse);
+
         }
     }
     public void animatorGestionner()
     {
         animator.SetFloat("Speed", Mathf.Abs( honrizontalMove));
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 }
